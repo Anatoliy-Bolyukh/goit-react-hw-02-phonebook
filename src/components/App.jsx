0096feffa48bf80d01b48e0c5373e5dfd7333cfc
user name <106239754+Anatoliy-Bolyukh@users.noreply.github.com>
2022-09-16 23:59:14 +0300

    add commit 03

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <>
+        <ContactForm />
+      </>
+    );
+  }
+}
